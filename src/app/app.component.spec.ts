6a41f5764398c77a28891415597341fd0e3e8036
Ross <lalanskyl@gmail.com>
2023-04-14 08:05:45 +0300

    Initialize project defaults, update README

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -30,6 +30,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ross-portfolio app is running!');
+    expect(compiled.querySelector('.test-content')?.textContent).toContain('ross-portfolio app is running!');
   });
 });
