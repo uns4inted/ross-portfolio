2c7aea04abc9e56c8800b62801ffadd8f699ba86
Ross <lalanskyl@gmail.com>
2023-04-15 18:52:39 +0300

    Add About page; Add icon packs dependencies; Update Styles;

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,10 +1,52 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// @ts-ignore
+import * as PureCounter from '@srexi/purecounterjs';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
+  public sectionShow: boolean = false;
+  public counter: PureCounter;
 
+  public skills = {
+    html: 0,
+    css: 0,
+    js: 0,
+    angular: 0,
+    react: 0,
+    node: 0,
+  };
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.showSection();
+    this.setSkills();
+    // initialize counter
+    this.counter = new PureCounter();
+  }
+
+  showSection() {
+    // show section after 300ms delay to play animation of section fade in
+    setTimeout(() => {
+      this.sectionShow = true;
+    }, 300);
+  }
+
+  setSkills() {
+    // set skills values after 300ms delay to play animation for progress bars
+    setTimeout(() => {
+      this.skills = {
+        html: 90,
+        css: 75,
+        js: 95,
+        angular: 85,
+        react: 75,
+        node: 50,
+      };
+    }, 300);
+  }
 }
