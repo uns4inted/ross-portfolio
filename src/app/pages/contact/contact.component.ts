33081e575c30c4ba63ab422add15ee215e32d2f6
Ross <lalanskyl@gmail.com>
2023-04-17 10:14:24 +0300

    Add Contact page with temp data

--- src/app/pages/contact/contact.component.ts
+++ src/app/pages/contact/contact.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
+  public sectionShow: boolean = false;
 
+  ngOnInit(): void {
+    this.showSection();
+  }
+
+  // TODO: extract to service
+  showSection() {
+    // show section after 300ms delay to play animation of section fade in
+    setTimeout(() => {
+      this.sectionShow = true;
+    }, 300);
+  }
 }
