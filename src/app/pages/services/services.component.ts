932692c8c877a77574c8a643d4be0c137858eefa
Ross <lalanskyl@gmail.com>
2023-04-17 10:01:26 +0300

    Add Services page with temp data; Add Boxicons package;

--- src/app/pages/services/services.component.ts
+++ src/app/pages/services/services.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-services',
   templateUrl: './services.component.html',
-  styleUrls: ['./services.component.scss']
+  styleUrls: ['./services.component.scss'],
 })
-export class ServicesComponent {
+export class ServicesComponent implements OnInit {
+  public sectionShow: boolean = false;
 
+  ngOnInit(): void {
+    this.showSection();
+  }
+
+  // TODO: extract to service
+  showSection() {
+    // show section after 300ms delay to play animation of section fade in
+    setTimeout(() => {
+      this.sectionShow = true;
+    }, 300);
+  }
 }
