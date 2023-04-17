3ac0d8abb7a226ffa87df9359661ad5b64f8a725
Ross <lalanskyl@gmail.com>
2023-04-17 10:34:58 +0300

    Add Portfolio page with temp data; Add mock pics for portfolio page;

--- src/app/pages/portfolio/portfolio.component.ts
+++ src/app/pages/portfolio/portfolio.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-portfolio',
   templateUrl: './portfolio.component.html',
-  styleUrls: ['./portfolio.component.scss']
+  styleUrls: ['./portfolio.component.scss'],
 })
-export class PortfolioComponent {
+export class PortfolioComponent implements OnInit {
+  public sectionShow: boolean = false;
 
+  ngOnInit(): void {
+    this.showSection();
+  }
+
+  // TODO: extract to service
+  showSection() {
+    // show section after 300ms delay to play animation of section fade in
+    setTimeout(() => {
+      this.sectionShow = true;
+    }, 300);
+  }
 }
